e76d330e4d14161da898dafa19832cf8f6420170
4pad7 <51042440+4pad7@users.noreply.github.com>
2023-07-20 12:00:44 +0800

    Update chatgpt.py

--- chatgpt.py
+++ chatgpt.py
@@ -3,7 +3,7 @@ from api.prompt import Prompt
 import os
 import openai
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = os.getenv("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")
 
 class ChatGPT:
     def __init__(self):
