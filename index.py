a3e64cd464827e06985385a33c34df968f6a7968
4pad7 <51042440+4pad7@users.noreply.github.com>
2023-07-20 11:56:17 +0800

    Update index.py

--- index.py
+++ index.py
@@ -3,25 +3,25 @@ from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 from api.chatgpt import ChatGPT
+
 import os
 
 line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
 line_handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
-working_status = os.getenv("DEFALUT_TALKING", default="true").lower() == "true"
+working_status = os.getenv("DEFALUT_TALKING", default = "true").lower() == "true"
+
 app = Flask(__name__)
 chatgpt = ChatGPT()
 
-
 # domain root
-@app.route("/")
+@app.route('/')
 def home():
-    return "Hello, World!"
+    return 'Hello, World!'
 
-
-@app.route("/webhook", methods=["POST"])
+@app.route("/webhook", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
-    signature = request.headers["X-Line-Signature"]
+    signature = request.headers['X-Line-Signature']
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
@@ -30,21 +30,25 @@ def callback():
         line_handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
-    return "OK"
+    return 'OK'
 
 
 @line_handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     global working_status
-
+    working_status = True
+    
     if event.message.type != "text":
         return
-    working_status = True
+    
+
     if working_status:
         chatgpt.add_msg(f"Human:{event.message.text}?\n")
         reply_msg = chatgpt.get_response().replace("AI:", "", 1)
         chatgpt.add_msg(f"AI:{reply_msg}\n")
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_msg))
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=reply_msg))
 
 
 if __name__ == "__main__":
